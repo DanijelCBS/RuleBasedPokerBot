707369cf6fee440d3f872eb0ac1d22f24a238f1f
Danijel Radulović <master.daca09@gmail.com>
2020-06-07 12:53:03 +0200

    Add Spring project for creating rules

---
+++ pokerbot/src/main/java/sbnz/rulebased/pokerbot/PokerbotApplication.java
@@ -0,0 +1,13 @@
+package sbnz.rulebased.pokerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerbotApplication.class, args);
+	}
+
+}
---
+++ pokerbot/src/test/java/sbnz/rulebased/pokerbot/PokerbotApplicationTests.java
@@ -0,0 +1,13 @@
+package sbnz.rulebased.pokerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
